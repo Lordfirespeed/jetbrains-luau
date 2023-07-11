53a92827f709a25c8acc39f86918b8203aff898c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-11 15:50:06 +0200

    add luau icon and icon holder class

---
+++ src/main/kotlin/com/github/lordfirespeed/jetbrainsluau/LuauIcons.kt
@@ -0,0 +1,8 @@
+package com.github.lordfirespeed.jetbrainsluau
+
+import com.intellij.openapi.util.IconLoader;
+
+object LuauIcons {
+    @JvmField
+    val LUAU = IconLoader.getIcon("/icons/luau.svg", javaClass)
+}
