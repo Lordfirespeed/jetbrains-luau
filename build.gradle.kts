0f9b8f4f817c44c3eedaa9ffa667c589671abbe5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-12 03:20:35 +0200

    use Antlr to generate lexer, parser, and visitor interfaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     id("java") // Java support
+    id(libs.plugins.antlr.get().pluginId) // Antlr plugin
     alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
@@ -24,6 +25,8 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
 //    implementation(libs.annotations)
+    antlr("org.antlr:antlr4:${properties("antlr4Version").get()}")
+    implementation("org.antlr:antlr4-intellij-adaptor:0.1")
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
@@ -64,6 +67,8 @@ koverReport {
     }
 }
 
+tasks.getByName("compileKotlin").dependsOn("generateGrammarSource");
+
 tasks {
     wrapper {
         gradleVersion = properties("gradleVersion").get()
@@ -79,7 +84,7 @@ tasks {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
@@ -122,6 +127,11 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+        channels =
+            properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+    }
+
+    generateGrammarSource {
+        arguments.plusAssign(listOf("-package", "com.github.lordfirespeed.jetbrainsluau.lang"))
     }
 }
